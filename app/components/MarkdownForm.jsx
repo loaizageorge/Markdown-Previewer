1d0519a7eda91f798d7b09d190c79c324dc584d3
George <loaizageorge@gmail.com>
2016-12-04 23:10:41 -0500

    Remove bug caused by setting value to text area

--- app/components/MarkdownForm.jsx
+++ app/components/MarkdownForm.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 
 var MarkdownForm = React.createClass({
   handleTyping: function(){
-    console.log('called');
     var input = document.getElementById('input').value;
     var markdown = marked(input);
     document.getElementById('output').innerHTML = markdown;
@@ -12,7 +11,7 @@ var MarkdownForm = React.createClass({
     //this.props.handleNewData(this.props.text);
     return(
       <div className = "small-12 medium-6 columns">
-        <textarea value = "# Markdown Previewer" id = "input" rows = "2" columns = "25" onChange = {this.handleTyping}></textarea>
+        <textarea id = "input" rows = "2" columns = "25" onChange = {this.handleTyping}></textarea>
       </div>
     );
 
