efd5f253e635bac58320ac4f466483d7d05e5a58
George <loaizageorge@gmail.com>
2016-12-08 22:53:38 -0500

    Finish style and add default text

--- app/components/Markdown.jsx
+++ app/components/Markdown.jsx
@@ -2,13 +2,11 @@ var React = require ('react');
 var MarkdownForm = require('MarkdownForm');
 var MarkdownOutput = require('MarkdownOutput');
 
-var render = new marked.Renderer();
-
 var Markdown = React.createClass({
 
   getInitialState: function(){
     return {
-    text:'## Markdown Viewer \n #### *with live preview* \n ____ \n Quick list of commands'
+    text:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[George Loaiza](https://github.com/xchendo/Markdown-Previewer)*'
     };
   },
   handleNewData: function(newText){
@@ -23,14 +21,33 @@ var Markdown = React.createClass({
   },
   render: function(){
     var text = this.state.text;
-    var markdown = this.translate();
+    var output = this.translate();
     return (
-      <div className = "container markdown-container">
+      <div>
+        <div className = "app-title">
+            <h1 className = "align">  Markdown Previewer <i className="devicon-react-original colored align"></i>  </h1>
+         </div>
+         <div className = "container">
+           <div className = "markdown-container">
+             <div className = "row">
+               <div className = "col-md-6">
+                  <h2 className = "title">Output</h2>
+                  <MarkdownOutput output = {output}/>
+               </div>
+               <div className = "col-md-6">
+                 <h2 className = "title">Input</h2>
+                 <MarkdownForm onNewData = {this.handleNewData} text = {text}/>
+               </div>
+             </div>
+           </div>
+         </div>
+      </div>
+      /*<div className = "container markdown-container">
         <div className = "row">
         <MarkdownOutput output = {markdown} />
         <MarkdownForm onNewData = {this.handleNewData} text = {text}/>
         </div>
-      </div>
+      </div> */
     );
   }
 });
--- app/components/MarkdownForm.jsx
+++ app/components/MarkdownForm.jsx
@@ -9,8 +9,8 @@ var MarkdownForm = React.createClass({
 
     //this.props.handleNewData(this.props.text);
     return(
-      <div className = "col-xs-12 col-sm-12 col-md-6">
-        <textarea value = {this.props.text} id = "input" rows = "10" columns = "2" onChange = {this.handleTyping}></textarea>
+      <div>
+        <textarea value = {this.props.text} id = "input" rows = "15" onChange = {this.handleTyping}></textarea>
       </div>
     );
 
